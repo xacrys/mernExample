27c2af97fb57918ca0743970e8af4395b31e96f8
xacrys <cristiantintinmedina@hotmail.com>
2020-06-14 22:52:36 -0500

    cambio 01 .- Eliminando Vistas

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Smartopia INC ' });
+  //res.render('index', { title: 'Smartopia INC ' });
+  res.send("<h1>Sin Vistas</h1>")
 });
 
 module.exports = router;
