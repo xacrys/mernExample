83476c81f93f2b2658cb7bc7a76385f8a2768793
xacrys <cristiantintinmedina@hotmail.com>
2020-06-14 23:16:36 -0500

    Eliminación de Jade y Pug

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var app = express();
 
 // view engine setup
 //app.set('views', path.join(__dirname, 'views'));
-//app.set('view engine', 'jade');
+//app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -24,8 +24,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 mongoConnector.connectToMongo();
 
-
-// routes 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  //res.render('index', { title: 'Smartopia INC ' });
-  res.send("<h1>Sin Vistas</h1>")
+  res.send('Inicio');
 });
 
 module.exports = router;
